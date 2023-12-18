e9c9ad48ed2f72f7b3c91f6cae6d93b9008d584d
Hafiz Alif Mahardika <115974935+0MZIFAH@users.noreply.github.com>
2023-12-19 03:50:08 +0700

    Update

--- cafe_db.sql
+++ cafe_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 12, 2020 at 02:18 PM
--- Server version: 10.5.5-MariaDB
--- PHP Version: 7.4.10
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Des 2023 pada 21.47
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,27 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ingredients`
+-- Struktur dari tabel `ingredients`
 --
 
 CREATE TABLE `ingredients` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   `stock` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
@@ -52,10 +37,18 @@ CREATE TABLE `ingredients` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `ingredients`
+--
+
+INSERT INTO `ingredients` (`id`, `name`, `stock`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
+(9, 'CUP', 10000, 10000, 10000, '2023-12-17 17:42:41', '2023-12-18 13:41:41'),
+(10, 'TEH', 500, -249525, 2500, '2023-12-17 17:43:14', '2023-12-18 04:27:02');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ingredient_menus`
+-- Struktur dari tabel `ingredient_menus`
 --
 
 CREATE TABLE `ingredient_menus` (
@@ -67,67 +60,90 @@ CREATE TABLE `ingredient_menus` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `ingredient_menus`
+--
+
+INSERT INTO `ingredient_menus` (`id`, `id_menu`, `id_ingredient`, `quantity`, `created_at`, `updated_at`) VALUES
+(15, 12, 9, 1, NULL, NULL),
+(16, 12, 10, 25, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menus`
+-- Struktur dari tabel `menus`
 --
 
 CREATE TABLE `menus` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `menus`
+--
+
+INSERT INTO `menus` (`id`, `name`, `price`, `photo`, `category`, `created_at`, `updated_at`) VALUES
+(12, 'ES TEH', 2500, 'images/menus/ToeZHUysvR40AyO5gMcfRpKunnqM7suLZ257Aask.jpg', 'Minuman', '2023-12-17 17:44:09', '2023-12-17 17:44:09');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Struktur dari tabel `migrations`
 --
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_resets`
+-- Struktur dari tabel `password_resets`
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Struktur dari tabel `transactions`
 --
 
 CREATE TABLE `transactions` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   `table_number` int(11) NOT NULL,
   `total_price` int(11) NOT NULL,
-  `transaction_status` enum('bayar','belum') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `transaction_status` enum('bayar','belum') NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `name`, `table_number`, `total_price`, `transaction_status`, `user_id`, `created_at`, `updated_at`) VALUES
+(10, 'Dika', 0, 2500, 'bayar', 1, '2023-12-17 17:44:38', '2023-12-17 17:44:46'),
+(11, 'Landung Widodo', 2, 25000000, 'bayar', 1, '2023-12-18 04:27:02', '2023-12-18 04:27:11');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction_details`
+-- Struktur dari tabel `transaction_details`
 --
 
 CREATE TABLE `transaction_details` (
@@ -139,142 +155,138 @@ CREATE TABLE `transaction_details` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `transaction_details`
+--
+
+INSERT INTO `transaction_details` (`id`, `id_transaction`, `id_menu`, `quantity`, `created_at`, `updated_at`) VALUES
+(20, 10, 12, 1, NULL, NULL),
+(21, 11, 12, 10000, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `level` enum('Kasir','Administrator') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `level` enum('Kasir','Administrator') NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `photo`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, 'Stiven Trizky Katuukk', 'pemudakoding', '$2a$12$fJIEf4gRPOqFmzSPsAS5FOeb4MPAmSWunG/Vq5cvxQVacIVIB.8HO', 'images/users/tFCMy0U7NzqNnfxutAfpWTYvl9OnJuD9fTxeOXM0.jpeg', 'Administrator', NULL, '2020-09-10 17:50:36', '2020-09-11 00:35:19'),
-(3, 'Rhisna Sandiaulia', 'rhisna', '$2a$12$fJIEf4gRPOqFmzSPsAS5FOeb4MPAmSWunG/Vq5cvxQVacIVIB.8HO', 'images/users/8afdKJpXvumJkzSTgpH3A3zh5yMzokzT2hIt7MLM.jpeg', 'Kasir', NULL, '2020-09-10 17:58:09', '2020-09-11 01:26:58');
+(2, 'Hafiz Alif M', 'mahardika', '$2y$10$9mAuRZaFc711QReFbcywMO1KVUSco6yznOJxO2DkXASriGj8sa9M.', 'images/users/uaAdtOvOLw7PtMhx6h85csBICEbFj9KPkh0bKgwA.jpg', 'Administrator', NULL, '2020-09-10 17:50:36', '2023-12-18 04:24:39'),
+(3, 'Azizil Yuda P', 'kasir', '$2y$10$k6PG8a3AWKQSKZjJP2xK0.IhniiIO63CYTPE8FZY0j2WIW2jSCF32', 'images/users/r2XazknyqFtIgAxXmxObuIO3HkdoZvbQfwGL1CmV.jpg', 'Kasir', NULL, '2020-09-10 17:58:09', '2023-12-18 04:26:13');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ingredients`
+-- Indeks untuk tabel `ingredients`
 --
 ALTER TABLE `ingredients`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `ingredient_menus`
+-- Indeks untuk tabel `ingredient_menus`
 --
 ALTER TABLE `ingredient_menus`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `menus`
+-- Indeks untuk tabel `menus`
 --
 ALTER TABLE `menus`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `migrations`
+-- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `password_resets`
+-- Indeks untuk tabel `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indexes for table `transactions`
+-- Indeks untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transaction_details`
+-- Indeks untuk tabel `transaction_details`
 --
 ALTER TABLE `transaction_details`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_username_unique` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `ingredients`
+-- AUTO_INCREMENT untuk tabel `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `ingredient_menus`
+-- AUTO_INCREMENT untuk tabel `ingredient_menus`
 --
 ALTER TABLE `ingredient_menus`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT for table `menus`
+-- AUTO_INCREMENT untuk tabel `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `transactions`
+-- AUTO_INCREMENT untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `transaction_details`
+-- AUTO_INCREMENT untuk tabel `transaction_details`
 --
 ALTER TABLE `transaction_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
